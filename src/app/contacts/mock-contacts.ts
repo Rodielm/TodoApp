6ba9f58ff6c12a26a812f4d40225cc035d847910
Rodiel Martinez <rodiel_martinez@claro.com.do>
2018-02-13 15:12:54 -0400

    added a function to contacts

---
+++ src/app/contacts/mock-contacts.ts
@@ -0,0 +1,14 @@
+import { Contact } from './contact';
+
+export const CONTACTS: Contact[] = [
+  { id: 11, name: 'Maria' },
+  { id: 12, name: 'Jose' },
+  { id: 13, name: 'Juan' },
+  { id: 14, name: 'Pedro' },
+  { id: 15, name: 'Luis' },
+  { id: 16, name: 'Rosa' },
+  { id: 17, name: 'Bella' },
+  { id: 18, name: 'Isabel' },
+  { id: 19, name: 'Ernesto' },
+  { id: 20, name: 'Julian' }
+];
